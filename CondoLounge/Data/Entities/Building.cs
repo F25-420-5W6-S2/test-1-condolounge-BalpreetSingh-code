397e53c81d4caff48d12854d29ea0baa7277fafa
BalpreetSingh-code <sahotabalpreetsingh1@gmail.com>
2025-11-18 12:19:29 -0500

    Added the entities for User, Building, Condo.

---
+++ CondoLounge/Data/Entities/Building.cs
@@ -0,0 +1,12 @@
+﻿namespace CondoLounge.Data.Entities
+{
+    public class Building
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string? Address { get; set; }
+
+        public ICollection <Condo> Condos { get; set; } = new List<Condo>();
+
+    }
+}
---
+++ CondoLounge/Data/Entities/Condo.cs
@@ -0,0 +1,15 @@
+﻿namespace CondoLounge.Data.Entities
+{
+    public class Condo
+    {
+        public int Id { get; set; }
+        public string CondoNumber { get; set; } = string.Empty;
+        public string Location { get; set; } = string.Empty;
+        public int BuildingId { get; set; }
+        public Building? Building { get; set; } = null!;
+        public ICollection<ApplicationUser> Users { get; set; } = new List<ApplicationUser>();
+
+
+
+    }
+}
